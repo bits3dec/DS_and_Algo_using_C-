a1f18958b29a6eddad9038fc1cc833649c4eb0f6
Preetom_Bhowmik <bits3dec@gmail.com>
2019-04-18 00:38:40 +0530

    Dynamic Programming

---
+++ Dynamic Programming/LevenshteinEditDistance/ConsoleApp1/ConsoleApp1/EditDistance.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+
+    // Operations: 1)No Edit 2)Delete 3)Replace 4)Insert
+    public class EditDistance
+    {
+        public int MinEdit(string s1, string s2, int m, int n)
+        {
+            //T[i,j] denotes the min number of edits required to transform s1(0,j-1) to s2(0,i-1)
+            int[,] T = new int[n+1, m+1];
+
+            for(int i = 0; i <= n; ++i)
+            {
+                for(int j = 0; j <= m; ++j)
+                {
+                    if(i == 0)
+                    {
+                        T[i, j] = j;//To transform s1(0,j-1)-> "" empty string we need to delete all the characters of s1
+                        continue;
+                    }
+                    if(j == 0)
+                    {
+                        T[i, j] = i;//To transform empty string "" -> s2(0,i-1)
+                        continue;
+                    }
+
+                    if (s1[j - 1] == s2[i - 1])
+                        T[i, j] = T[i - 1, j - 1]; //No edit operation
+                    else
+                    {
+                        T[i, j] = Min( 1 + T[i, j - 1],      //Delete
+                                       1 + T[i - 1, j - 1],  //Replace
+                                       1 + T[i - 1, j]       //Insert
+                                     );     
+                    }
+                }
+            }
+
+            return T[n, m];
+        }
+
+        private int Min(int x, int y, int z)
+        {
+            return Math.Min(x, Math.Min(y, z));
+        }
+    }
+}
---
+++ Dynamic Programming/LevenshteinEditDistance/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "sunday";
+            string str2 = "saturday";
+
+            EditDistance calculator = new EditDistance();
+
+            int res = calculator.MinEdit(str1, str2, str1.Length, str2.Length);
+
+            Console.WriteLine($"The minimum edit operations need to transform \"{str1}\" to \"{str2}\" is: {res}");
+
+            Console.ReadKey();
+        }
+    }
+}
