7d5e15c621c5766283be2b9e4388c252bcd54fbf
Preetom_Bhowmik <bits3dec@gmail.com>
2019-04-20 01:33:37 +0530

    Dynamic Programming

---
+++ Dynamic Programming/OptimalGameStrategy/ConsoleApp1/ConsoleApp1/OptimalGameStrategy.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class OptimalGameStrategy
+    {
+        public static int MaximumValue(int[] arr)
+        {
+            int n = arr.Length;
+
+            //T[i,j] denotes the maximum value the user can get from the array of coins arr[i]...arr[j] if he picks first
+            int[,] T = new int[n, n];
+
+            for(int gap = 0; gap < n; ++gap)
+            {
+                for(int i = 0, j = gap; j < n; ++i, ++j)
+                {
+                    if (i == j)
+                        T[i, j] = arr[i];
+                    else if (i + 1 == j)
+                        T[i, j] = Math.Max(arr[i], arr[j]);
+                    else
+                    {
+                        T[i, j] = Math.Max(arr[i] + Math.Min(T[i + 1, j - 1], T[i + 2, j]),
+                                           arr[j] + Math.Min(T[i + 1, j - 1], T[i, j - 2])
+                                           );
+                    }
+                }
+            }
+
+            return T[0, n - 1];
+        }
+    }
+}
---
+++ Dynamic Programming/OptimalGameStrategy/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = { 8, 15, 3, 7 };
+            int n = arr1.Length;
+            Console.WriteLine("" + OptimalGameStrategy.MaximumValue(arr1));
+
+            int[] arr2 = { 2, 2, 2, 2 };
+            Console.WriteLine("" + OptimalGameStrategy.MaximumValue(arr2));
+
+            int[] arr3 = { 20, 30, 2, 2, 2, 10 };
+            Console.WriteLine("" + OptimalGameStrategy.MaximumValue(arr3));
+
+            Console.ReadKey();
+        }
+    }
+}
