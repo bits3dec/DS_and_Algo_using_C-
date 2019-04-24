8f8223dab72ecfe70da5fb77e30488fa4b268bbd
Preetom_Bhowmik <bits3dec@gmail.com>
2019-04-24 17:44:17 +0530

    Refactored MaximumContiguousSum

---
+++ Dynamic Programming/MaximumContiguousSum/MaximumContiguousSum/MaximumContiguousSum/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximumContiguousSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { -2, -3, 4, -1, -2, 1, 5, -3 };
+
+            MaxContiguousSumUtility.MaxContiguousSum(arr);
+            MaxContiguousSumUtility_DP.MaxContiguousSum(arr);
+
+            Console.ReadKey();
+        }
+    }
+}
