bb7a10c44002c764962fbed1e9c562598732dad2
Preetom_Bhowmik <bits3dec@gmail.com>
2019-05-02 09:07:18 +0530

    Graph Theory

---
+++ Graphs/Hamiltonian Cycle Backtracking/ConsoleApp1/ConsoleApp1/HamiltonCycleUtil.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class HamiltonianUtil
+    {
+        //T:O(n!) Backtracking: NP Hard Problem
+        public static void CheckHamiltonCycle(int[,] g, int N, int s)
+        {
+            int[] path = new int[N];
+
+            for (int i = 0; i < N; ++i)
+                path[i] = -1;
+
+            path[0] = s;//Fix the start index as vertex "s" since if it is a cycle the starting point doesnot matter
+
+            if (HamiltonCycleUtil(g, N, path, 1) == true)
+                PrintPath(path, N);
+            else
+                Console.WriteLine("No Hamilton Cycle");
+        }
+
+        private static bool HamiltonCycleUtil(int[,] g, int N, int[] path, int pos)
+        {
+            if(pos == N)
+            {
+                if (g[path[pos - 1], 0] == 1)
+                    return true;
+
+                return false;
+            }
+
+            //Check for all the vertices
+            for(int v = 0; v < N; ++v)
+            {
+                if(IsSafe(g, N, path, pos, v) == true)
+                {
+                    path[pos] = v;
+
+                    if (HamiltonCycleUtil(g, N, path, pos + 1) == true)
+                        return true;
+
+                    path[pos] = -1;//Remove the path[pos] if it doesnot lead to a solution
+                }
+            }
+            return false;
+        }
+
+        private static bool IsSafe(int[,] g, int N, int[] path, int pos, int v)
+        {
+            //check if there exist an edge from previous vertex in the path to this vertex
+            if (g[path[pos - 1], v] == 0)
+                return false;
+
+            //check if the vertex already exist in the path
+            for (int i = 0; i < N; ++i)
+                if (path[i] == v)
+                    return false;
+
+            return true;
+        }
+
+        private static void PrintPath(int[] path, int N)
+        {
+            for (int i = 0; i < N; ++i)
+                Console.Write(path[i] + " ");
+
+            Console.Write(path[0]);
+            Console.WriteLine();
+
+        }
+    }
+}
--- Graphs/Hamiltonian Cycle Backtracking/ConsoleApp1/ConsoleApp1/Program.cs
+++ Graphs/Hamiltonian Cycle Backtracking/ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,29 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            int N = 0;
+            int s = 0;
+            int[,] graph1 = {{0, 1, 0, 1, 0},
+                             {1, 0, 1, 1, 1},
+                             {0, 1, 0, 0, 1},
+                             {1, 1, 0, 0, 1},
+                             {0, 1, 1, 1, 0},
+                            };
+            N = graph1.GetLength(0);
+            s = 0;//Start index is vertex 0
+            HamiltonianUtil.CheckHamiltonCycle(graph1, N, s);
+
+            int[,] graph2 = {{0, 1, 0, 1, 0},
+                             {1, 0, 1, 1, 1},
+                             {0, 1, 0, 0, 1},
+                             {1, 1, 0, 0, 0},
+                             {0, 1, 1, 0, 0},
+                            };
+            N = graph1.GetLength(0);
+            s = 0;
+            HamiltonianUtil.CheckHamiltonCycle(graph2, N, s);
+
+            Console.ReadKey();
         }
     }
 }
