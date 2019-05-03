3173ca4209b8bf5ed2875b557f2b20fc94482e6a
Preetom_Bhowmik <bits3dec@gmail.com>
2019-05-03 08:53:06 +0530

    [Graph Theory] Travelling Salesman Problem

---
+++ Graphs/Travelling Salesman Problem/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] graph = {{ 0, 10, 15, 20 }, 
+                            { 10, 0, 35, 25 }, 
+                            { 15, 35, 0, 30 }, 
+                            { 20, 25, 30, 0 } 
+                           };
+
+            TravellingSalesmanUtility.Tsp(graph);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Graphs/Travelling Salesman Problem/ConsoleApp1/ConsoleApp1/TravellingSalesmanUtility.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class TravellingSalesmanUtility
+    {
+        //T:O(n!)
+        public static void Tsp(int[,] g)
+        {
+            int n = g.GetLength(0);
+            bool[] visited = new bool[n];
+            int ans = Int32.MaxValue;
+
+            visited[0] = true;
+            TspUtil(g, 1, 0, 0, ref ans, visited, n);
+
+            if(ans == Int32.MaxValue)
+                Console.WriteLine("No path exist");
+            else
+                Console.WriteLine(ans);
+        }
+
+        private static void TspUtil(int[,] g, int count, int currPos, int cost, ref int ans, bool[] visited, int n)
+        {
+            if(count == n)
+            {
+                if(g[currPos, 0] != 0)
+                {
+                    ans = Math.Min(ans, cost + g[currPos, 0]);
+                    return;
+                }
+            }
+            else
+            {
+                for(int i = 0; i < n; ++i)
+                {
+                    if(g[currPos, i] != 0 && visited[i] == false)
+                    {
+                        visited[i] = true;
+                        TspUtil(g, count+1, i, cost + g[currPos, i], ref ans, visited, n);
+                        visited[i] = false;
+                    }
+                }
+            }
+        }
+    }
+}
